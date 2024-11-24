910ef5aeb928436d675c283568c6eba99f62ec5d
KangSohyun <nguyenthinhung29022004@gmail.com>
2024-11-25 00:18:15 +0700

    Add a feature for demonstration

--- src/Aims/Aims.java
+++ src/Aims/Aims.java
@@ -1,25 +1,18 @@
 package Aims;
 
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
 public class Aims {
 
-    public static void main(String[] args) {    
-        Cart anOrder = new Cart();
+	public static void main(String[] args) { // Tạo các đối tượng DVD mới
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Cinderella", "Fantasy", 14.99f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Star Wars", "Sci-Fi", "George Lucas", 150, 24.99f);
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
-
-        // Thêm các DVD vào giỏ hàng
-        anOrder.addDigitalVideoDisc(dvd1);
-        anOrder.addDigitalVideoDisc(dvd2, dvd3);  // Thêm nhiều DVD
-
-        // Hiển thị tổng chi phí và giỏ hàng
-        System.out.println("Total Cost is: " + anOrder.totalCost());
-        anOrder.displayCart();
-
-        // Xóa một DVD khỏi giỏ hàng
-        anOrder.removeDigitalVideoDisc(dvd3);
-        anOrder.displayCart();
-    }
+		// In ra số lượng DVD đã tạo và ID của mỗi DVD
+		System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+		System.out.println("DVD 1 ID: " + dvd1.getId());
+		System.out.println("DVD 2 ID: " + dvd2.getId());
+		System.out.println("DVD 3 ID: " + dvd3.getId());
+	}
 }
-
